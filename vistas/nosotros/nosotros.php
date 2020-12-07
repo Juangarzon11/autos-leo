620ebc502159f945e9854ad09ed75e49842e2963
Juangarzon11 <juangarzon07@gmail.com>
2020-12-07 09:19:32 -0500

    Vista nosotros

--- vistas/nosotros/nosotros.php
+++ vistas/nosotros/nosotros.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Autos Leo</title>
-
+    <title>Nosotros</title>
     <!--importacion boostrap-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -16,16 +14,15 @@
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/f599362e7b.js" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="../../css/estilo.css">
 </head>
-
 <body>
 
-    <section class="bg-ind">
+<section class="">
         <nav class="navbar navbar-expand-lg navbar-light bg-nav">
             <div class="col-sm-3 text-center">
                 <a class="navbar-brand" href="index.php">
-                    <img src="img/logo-color.png" alt="">
+                    <img src="../../img/logo-color.png" alt="">
                 </a>
             </div>
 
@@ -35,10 +32,10 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav hola ml-auto">
                     <li class="nav-item ">
-                        <a class="nav-link" href="#">Inicio</span></a>
+                        <a class="nav-link" href="../../">Inicio</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="vistas/nosotros/nosotros.php">¿Quienes somos?</a>
+                        <a class="nav-link" href="#">¿Quienes somos?</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Productos</a>
@@ -51,8 +48,8 @@
                             <button class="dropdown-item" type="button">Mi Perfil</button>
                             <button class="dropdown-item" type="button">Mi Vehículo</button>
                             <button class="dropdown-item" type="button">Reservas</button>
-                            <a href="vistas/Usuario/form_cliente.php"><button class="dropdown-item" type="button">(Cliente)</button></a>
-                            <a href="vistas/proveedor/form_proveedor.php"><button class="dropdown-item" type="button">(Proveedor)</button></a>
+                            <a href="../Usuario/form_cliente.php"><button class="dropdown-item" type="button">(Cliente)</button></a>
+                            <a href="../proveedor/form_proveedor.php"><button class="dropdown-item" type="button">(Proveedor)</button></a>
                             <div class="dropdown-divider"></div>
                             <button class="dropdown-item" type="button">Cerrar Sesión</button>
                         </div>
@@ -62,17 +59,48 @@
         </nav>
     </section>
 
-    <section class="">
-    <br>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod accumsan sagittis. Maecenas quam ante, fringilla eu posuere sit amet, porta id libero. Nam eleifend magna venenatis nisl luctus dignissim. Vivamus ornare posuere laoreet. Nullam pharetra volutpat ex vel blandit. Nam rutrum feugiat ante ac sodales. Vestibulum dignissim sapien tellus. Curabitur pulvinar ante augue, eget lacinia ante aliquet quis. In non tristique nibh, nec maximus libero. Nunc a nisi volutpat, mattis quam vitae, sodales enim. Aenean gravida ultrices ligula eget pellentesque.
 
-Praesent suscipit et neque congue finibus. Pellentesque accumsan accumsan enim mattis elementum. Nulla condimentum neque ex, sed ultrices ex facilisis ac. Pellentesque sodales, lectus vitae laoreet posuere, dui leo venenatis urna, ut blandit urna ante nec ante. Duis ac vulputate lorem. Ut vehicula nunc massa, quis maximus quam eleifend id. Aenean vestibulum arcu urna. Donec hendrerit ex a dolor pellentesque lacinia. Curabitur ante urna, commodo a vulputate in, pulvinar sed est. Cras at ultrices magna, eu pulvinar sapien. Sed mattis eu lorem non condimentum. Mauris vitae metus ligula. Curabitur luctus porta porta. Morbi dictum ipsum sapien, id sollicitudin est auctor non. Aliquam quis nisi eu nulla semper posuere in ultricies velit. Integer porta laoreet hendrerit.
+    
+    <main>
+        <div class="container my-5">
+            <div class="row col-12">
+                <div class="tarjetas p-md-3 col-12 col-md-4">
+                    <div class="tar-img">
+                        <img src="../../img/nosotros.svg" alt="">
+                    </div>
+                    <h3>¿Quiénes somos?</h3>
+                    <p>AUTOS LEO  ofrece servicios de mantenimiento preventivo y correctivo de vehículos, basado en la objetividad y profesionalismo de sus empleados y de la tecnología utilizada, primando la ética, honestidad y responsabilidad del trabajo realizado, buscando la satisfacción de nuestros clientes proveedores y el crecimiento de nuestra organización como una gran fuente de ingreso de su propietario, atravesar de un mejoramiento continuo de sus procesos .</p>
+                </div>
+                <div class="tarjetas p-md-3 col-12 col-md-4">
+                    <div class="tar-img">
+                        <img src="../../img/mision.svg" alt="">
+                    </div>
+                    <h3>Misión</h3>
+                    <p>Es misión de la empresa prestar un servicio óptimo y oportuno que les
+                        garantice a nuestros clientes el mejor desempeño de sus automotores,
+                        contando con personal capacitado y competente para brindar una atención
+                        personalizada ante cualquier eventualidad.  </p>
+                </div>
+                <div class="tarjetas p-md-3 col-12 col-md-4">
+                    <div class="tar-img">
+                        <img src="../../img/vision.svg" alt="">
+                    </div>
+                    <h3>Visión</h3>
+                    <p>Para el 2021 consolidarnos como una de las empresas líderes en el mercado
+                        prestando los servicios en mantenimientos preventivos y correctivos en la
+                        línea automotriz.
+                        Estamos enfocados al crecimiento diario, por eso somos una empresa de
+                        proyección, capaz de redefinir el concepto de calidad y garantía que se ofrece
+                        en la actualidad.
+                        Crecer a la par de los avances tecnológicos, para un continuo mejoramiento
+                        y cubrimiento a nivel de servicio, enmarcara que la infraestructura interna sea
+                        modificada, que el personal se capacite constantemente y cubra la demanda
+                        personalizada del servicio. </p>
+                </div>
+            </div>
+        </div>
+    </main>
 
-Nullam at diam sollicitudin, iaculis sapien nec, blandit est. Nunc orci dolor, porta at lobortis sit amet, lacinia eu ante. Quisque varius ac dolor vel cursus. Phasellus a tortor sodales quam mollis rutrum ut non ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed eget ligula ac felis ullamcorper varius eu eu elit. Aenean non eros ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum iaculis velit quis eleifend congue. Duis accumsan finibus tortor eget tempus. Sed volutpat aliquet mi et rhoncus. Curabitur egestas commodo mi, ut pretium turpis. Integer sit amet viverra turpis, id egestas tellus. Donec quis placerat ligula. Nulla pharetra elit nibh. In finibus, justo aliquam blandit pellentesque, enim tortor vestibulum magna, eu venenatis massa lacus ut mauris.
-
-Pellentesque facilisis viverra dui, quis egestas dolor tristique sed. Pellentesque congue posuere lectus tristique feugiat. Vivamus molestie blandit est a efficitur. Duis facilisis scelerisque turpis, ultrices posuere nunc sagittis in. Vivamus eros mi, bibendum vel scelerisque non, luctus a risus. Sed sed sapien dapibus, accumsan lectus sed, ultricies metus. Pellentesque nec nisl dapibus massa varius suscipit. Maecenas et commodo dui. Fusce cursus augue odio, at mattis diam egestas quis. </p>
-         <br>
-    </section>
 
     <footer class="footer py-4 ">
         <div class="container">
@@ -82,7 +110,7 @@ Pellentesque facilisis viverra dui, quis egestas dolor tristique sed. Pellentesq
                 </div>
                 <div class="col-lg-6 text-lg-center text-center">
                     <a class="navbar-brand" href="index.php">
-                        <img src="img/logo-blan.png" alt="">
+                        <img src="../../img/logo-blan.png" alt="">
                     </a>
                 </div>
                 <div class="col-lg-3 my-3 my-lg-0 text-lg-center text-center">
@@ -94,6 +122,7 @@ Pellentesque facilisis viverra dui, quis egestas dolor tristique sed. Pellentesq
         </div>
     </footer>
 
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
@@ -101,5 +130,4 @@ Pellentesque facilisis viverra dui, quis egestas dolor tristique sed. Pellentesq
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 </body>
-
 </html>
