ce9cf43ff95212c34d668fd0ef43600071831f33
sam-oro <samiroroz@gmail.com>
2021-01-14 09:28:42 -0500

    Proveedores

--- vistas/proveedores/lista-proveedores/vista-proveedor.php
+++ vistas/proveedores/lista-proveedores/vista-proveedor.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
+    <title>Proveedores</title>
+    
     <!--importacion boostrap-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -16,17 +15,51 @@
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/f599362e7b.js" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="../../css/estilo.css">\
-
+    <link rel="stylesheet" href="../../../css/estilo.css">
 
 </head>
-
 <body>
 
-    <div class="container col-12 d-flex">
+<div class="container">
 
-        <div class="card col-auto" style="width: 18rem;">
-            <img class="card-img-top" src="../../img/bg-01.jpg" alt="Card image cap">
+
+    <section>
+
+        <h1>Proveedores</h1>
+
+    </section>
+
+</div>
+    
+    
+    <div class="container">
+        <div class="row2">
+            
+        <div class="card col-auto m-3" style="width: 18rem;">
+            <img class="card-img-top" src="../../../img/bg-01.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h3 class="card-title text-center">Card title</h3>
+                <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush text-center precio-productos">
+                <hr>
+                <button class="btn btn-color mb-3 mt-0"> Ver más</button>
+                <!-- <li class="list-group-item"><i class="fas fa-tag"></i> $4.000</li> -->
+            </ul>
+        </div>
+
+        <div class="card col-auto m-3" style="width: 18rem;">
+            <img class="card-img-top" src="../../../img/bg-01.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h3 class="card-title text-center">Card title</h3>
+                <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush text-center precio-productos">
+                <li class="list-group-item"><i class="fas fa-tag"></i> $4.000</li>
+            </ul>
+        </div>
+        <div class="card col-auto m-3" style="width: 18rem;">
+            <img class="card-img-top" src="../../../img/bg-01.jpg" alt="Card image cap">
             <div class="card-body">
                 <h3 class="card-title text-center">Card title</h3>
                 <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -36,8 +69,8 @@
             </ul>
         </div>
 
-        <div class="card col-auto" style="width: 18rem;">
-            <img class="card-img-top" src="../../img/bg-01.jpg" alt="Card image cap">
+        <div class="card col-auto m-3" style="width: 18rem;">
+            <img class="card-img-top" src="../../../img/bg-01.jpg" alt="Card image cap">
             <div class="card-body">
                 <h3 class="card-title text-center">Card title</h3>
                 <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -47,43 +80,8 @@
             </ul>
         </div>
 
-        <div class="card col-auto" style="width: 18rem;">
-            <img class="card-img-top" src="../../img/bg-01.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h3 class="card-title text-center">Card title</h3>
-                <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush text-center precio-productos">
-                <li class="list-group-item"><i class="fas fa-tag"></i> $4.000</li>
-            </ul>
         </div>
-
-        <div class="card col-auto" style="width: 18rem;">
-            <img class="card-img-top" src="../../img/bg-01.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h3 class="card-title text-center">Card title</h3>
-                <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush text-center precio-productos">
-                <li class="list-group-item"><i class="fas fa-tag"></i> $4.000</li>
-            </ul>
-        </div>
-
-        <div class="card col-auto" style="width: 18rem;">
-            <img class="card-img-top" src="../../img/bg-01.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h3 class="card-title text-center">Card title</h3>
-                <p class="card-text text-justify">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush text-center precio-productos">
-                <li class="list-group-item"><i class="fas fa-tag"></i> $4.000</li>
-            </ul>
-        </div>
-
-
-
     </div>
-
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
@@ -92,5 +90,4 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 </body>
-
 </html>
